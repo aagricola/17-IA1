3a81c76391154b4fbe5eb3deaa943f7de481a0f9
Amanda Agricola <amandaagricola@Amandas-MacBook-Pro-3.local>
2017-10-04 17:59:06 -0400

    syllabus, for-counter

--- week-6_Loops/code/06_for-counter/sketch.js
+++ week-6_Loops/code/06_for-counter/sketch.js
@@ -11,12 +11,12 @@ function draw() {
   background(0);
   strokeWeight(random(4));
   stroke(255);
-  
+
   // Start the counter (i) at 0 and keep adding 1 while it is under 10
   // While that is true, do what is between {}
-   for (var i = 0; i < 10; i++){ 
+   for (var i = 0; i < 10; i++){
     noFill();
     rect(random(width) , random(height), random(40), random(40));
     }
-    
+
 }
