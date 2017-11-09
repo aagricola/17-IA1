3b5ff28444d4718fcf2500fa69ef9820f64db876
Amanda Agricola <amandaagricola@Amandas-MacBook-Pro-3.local>
2017-11-08 19:05:42 -0500

    analog-serial-out

--- week-11_Analog-Serial/code/analog-serial-output/analog_LED/analog_LED.ino
+++ week-11_Analog-Serial/code/analog-serial-output/analog_LED/analog_LED.ino
@@ -1,13 +1,11 @@
-// Basic analog input for use with potentiometers, photocells, etc.
-
-int analog0; // A variable to hold analog input. Analog input will always be a range of numbers between 0~1023
-
-void setup(){
-  // Neither analog input nor output uses pinMode()!
+void setup() {
+ Serial.begin(9600);           // initialize serial communications
 }
-
-void loop(){
-  analog0 = analogRead(0); // Read input from the specified analog pin.
-  analog0 = map(analog0, 0, 1023, 0, 255); // Map range of input (0-1023) to match range of output (0-255)
-  analogWrite(3, analog0); // The 2 refers to digital pin 3. Only the digital pins with a ~ next to them can be used for analog output.
+ 
+void loop() {
+ if (Serial.available() > 0) { // if there's serial data available
+ int inByte = Serial.read();   // read it
+ Serial.write(inByte);         // send it back out as raw binary data
+ analogWrite(5, inByte);       // use it to set the LED brightness
+ }
 }
